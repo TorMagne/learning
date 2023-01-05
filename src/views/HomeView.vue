ee57795caefc7fa0787b39da0a028add0c6410e8
tor <tormage@hotmail.com>
2023-01-05 12:59:09 +0100

    added tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
   </main>
 </template>
